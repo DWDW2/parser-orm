d667cc1bb25f56bbbfdd2f2c3446698bdd548c57
Zhansar <daid8002@gmail.com>
2025-01-15 21:42:03 +0500

    fixed tokenizer

--- __test__/main.js
+++ __test__/main.js
@@ -2,6 +2,4 @@ import { Parser } from "../parser.js";
 
 const parser = new Parser();
 
-console.log(JSON.stringify(parser.parse(` 45  `), null, 2));
-
-console.log(/^\s+/.exec("     45   "));
+console.log(JSON.stringify(parser.parse(`        45`), null, 2));
--- tokenizer.js
+++ tokenizer.js
@@ -1,8 +1,6 @@
 /**
  * Tokenizer spec.
- * Here it will be matching regex
- * and will execute it based on matching
- *
+ * It defines the regular expressions that will be parsed using this parser
  */
 
 const Spec = [
@@ -20,9 +18,6 @@ class Tokenizer {
     this._string = string;
     this._cursor = 0;
   }
-  /**
-   * here i need to impement getNextToken fumction
-   */
 
   isEOF() {
     return this._cursor === this._string.length;
@@ -40,10 +35,13 @@ class Tokenizer {
     const string = this._string.slice(this._cursor);
     //Generic tokenizer impementation
     for (const [regex, tokenType] of Spec) {
+      console.log(`Trying regex: ${regex} on string: ${string}`);
       const tokenValue = this._match(regex, string);
       if (tokenValue == null) {
         continue;
       }
+      this._cursor += tokenValue.length;
+      // Skip tokens that should be ignored (e.g., whitespace)
       if (tokenType == null) {
         return this.getNextToken();
       }
@@ -57,7 +55,7 @@ class Tokenizer {
   }
 
   _match(regex, string) {
-    const match = regex.exec(string);
+    const match = new RegExp(regex).exec(string);
     if (match == null) {
       return null;
     }
